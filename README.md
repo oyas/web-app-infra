444a10d452dcc77d72fea78c357367938117c4e6
oyas <poteto83@gmail.com>
2021-02-17 02:25:19 +0900

    Add nginx-internal for proxy admin tools

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ Kubernetes manifests of [web-app](https://github.com/oyas/web-app).
 bash cluster/local/setup.sh
 ```
 
+After creating, you can access to http://localhost:8080/
+
+#### Admin tools
+
+```
+bash cluster/local/port-forward.sh
+```
+
+- [Argo CD](localhost:20000)
+- [Consul](localhost:20001)
+- [Grafana](localhost:20002)
+- [Alertmanager](localhost:20003)
+- [Prometheus](localhost:20004)
+
---
+++ cluster/local/port-forward.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl port-forward deployment/nginx-internal 20000:20000 &  # argocd
+kubectl port-forward deployment/nginx-internal 20001:20001 &  # consul
+kubectl port-forward deployment/nginx-internal 20002:20002 &  # grafana
+kubectl port-forward deployment/nginx-internal 20003:20003 &  # alertmanager
+kubectl port-forward deployment/nginx-internal 20004:20004 &  # prometheus
+
+wait
