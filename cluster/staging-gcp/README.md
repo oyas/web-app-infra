b6aff6ac7001233af305edafcd7b4843118ea0b0
oyas <poteto83@gmail.com>
2021-04-22 23:08:38 +0900

    Add new cluster: staging-gcp

---
+++ cluster/staging-gcp/README.md
@@ -0,0 +1,7 @@
+Setup cluster on google kubernetes engine
+
+# Register Static IP
+
+Requires a static IP named `web-app-staging-ip`.
+https://cloud.google.com/kubernetes-engine/docs/tutorials/configuring-domain-name-static-ip
+
---
+++ cluster/staging-gcp/setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# Setup
+#   $ bash setup.sh
+#
+
+function waitfor(){
+	name=$1
+	namespace=$2
+
+	echo "Waiting to $name ready..."
+	kubectl get pods -l app.kubernetes.io/name=$name -n $namespace 2>/dev/null | sed -n '1p'
+	for i in {1..600}; do
+		stat=`kubectl get pods -l app.kubernetes.io/name=$name -n $namespace 2>/dev/null | sed -n '2p' | tr -d '\n'`
+		echo -en "\r$stat"
+		if [[ $stat == *Running* ]]; then
+			echo ''
+			break
+		fi
+		sleep 1
+	done
+}
+
+# Move to this script dir
+cd `dirname $0`
+
+# Check requirements
+if !(type "kubectl" > /dev/null 2>&1); then
+	echo kubectl is not found. Please install.
+	echo https://kubernetes.io/docs/tasks/tools/install-kubectl/
+	exit 1
+fi
+
+# Install Argo CD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Set admin password as 'admin'
+kubectl -n argocd patch secret argocd-secret \
+	-p '{"stringData": {
+		"admin.password": "$2a$10$HvC0G7RMxR0PhEtZ9pFWseChNQH9EQcOByn2hFtuhdAlK0tvS4gD2",
+		"admin.passwordMtime": "'$(date +%FT%T%Z)'"
+	}}'
+
+# Wait argocd-server ready
+waitfor argocd-server argocd
+
+# Add Argo CD root application
+kubectl apply -n argocd -f ./application.yaml
+
+echo ""
+echo "Ready to use Argo CD! Please run following command."
+echo ""
+echo "  $ kubectl port-forward svc/argocd-server -n argocd 20000:443"
+echo ""
+echo "Then, you can use Argo CD at http://localhost:20000/"
+echo "Note: 'port-forward.sh' is not available before all services ready."
+
